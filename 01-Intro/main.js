24cb752a53cdf44056f812ff6daa0661a9bb729a
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2023-04-05 18:21:19 -0300

    Install electron packager

--- 01-Intro/main.js
+++ 01-Intro/main.js
@@ -14,3 +14,11 @@ function createMainWindow(){
 }
 
 app.whenReady().then(createMainWindow)
+
+app.on('activate', ()=>{
+    if(BrowserWindow.getAllWindows().length === 0) createMainWindow()
+})
+
+app.on('window-all-closed',()=>{
+    if(process.platform !== 'darwin') app.quit()
+})
